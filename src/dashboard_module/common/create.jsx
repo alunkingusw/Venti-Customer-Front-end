19e8da73bb72565706b8c176331f810df1bebcfe
evanskosgei <evanskosgei090@gmail.com>
2024-06-20 15:10:00 +0300

    imoji selector and submit

--- src/dashboard_module/common/create.jsx
+++ src/dashboard_module/common/create.jsx
@@ -3,7 +3,10 @@
 import React, { useState } from 'react';
 import { BsX } from 'react-icons/bs';
 import { IoImagesOutline } from 'react-icons/io5';
+import { CiFaceSmile } from "react-icons/ci";
+import { FaRegImage } from "react-icons/fa6";
 import Cropper from 'react-easy-crop';
+import EmojiPicker from 'emoji-picker-react';
 import getCroppedImg from './cropImage';
 
 const Create = ({ closeCreate }) => {
@@ -15,6 +18,8 @@ const Create = ({ closeCreate }) => {
     const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
     const [croppedImage, setCroppedImage] = useState(null)
     const [error, setError] = useState('');
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+    const [textAreaContent, setTextAreaContent] = useState('');
 
     const onCropComplete = (croppedArea, croppedAreaPixels) => {
         setCroppedAreaPixels(croppedAreaPixels)
@@ -45,6 +50,14 @@ const Create = ({ closeCreate }) => {
         } catch (error) {
             console.error('Error cropping image:', error);
         }
+    };
+    const pickEmoji = () => {
+        setShowEmojiPicker(!showEmojiPicker);
+    };
+    const onEmojiClick = (code, emojiObject) => {
+        console.log('emojiObject:', code.emoji);
+        setTextAreaContent(prevContent => prevContent + code.emoji);
+        setShowEmojiPicker(false);
     };
 
     return (
@@ -153,6 +166,37 @@ const Create = ({ closeCreate }) => {
                                         <IoImagesOutline className="w-40 h-40 text-gray-500 dark:text-gray-400 mb-10" />
                                     )}
                                 </div>
+                                {/* <form> */}
+                                <label htmlFor="chat" className="sr-only">Add a Caption</label>
+                                <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
+                                    <button type="button" className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                        <FaRegImage className="w-5 h-5" aria-hidden="true" />
+                                        <span className="sr-only">Upload image</span>
+                                    </button>
+                                    <button
+                                        onClick={pickEmoji}
+                                        type="button" className="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                        <CiFaceSmile className="w-5 h-5" />
+                                        <span className="sr-only">Add emoji</span>
+                                    </button>
+                                    <textarea
+                                        id="chat" rows="1"
+                                        value={textAreaContent}
+                                        onChange={(e) => setTextAreaContent(e.target.value)}
+                                        className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Your message..."></textarea>
+                                    <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                                        <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                            <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
+                                        </svg>
+                                        <span className="sr-only">Send message</span>
+                                    </button>
+                                </div>
+                                {showEmojiPicker && (
+                                    <div className="absolute bottom-20">
+                                        <EmojiPicker onEmojiClick={onEmojiClick} />
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </div>
