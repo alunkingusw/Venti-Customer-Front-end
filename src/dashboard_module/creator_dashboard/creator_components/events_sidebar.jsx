24cd97370fbfcf5fb88a71b12576d75cbcf2dcd1
evanskosgei <evanskosgei090@gmail.com>
2024-09-02 13:14:20 +0300

    mobile view

--- src/dashboard_module/creator_dashboard/creator_components/events_sidebar.jsx
+++ src/dashboard_module/creator_dashboard/creator_components/events_sidebar.jsx
@@ -7,26 +7,25 @@ import { MdOutlineEventRepeat, MdEventAvailable, MdEvent, MdOutlineSummarize } f
 const Events_sidebar = () => {
     return (
         <div className=''>
-            <div className="p-2 border-b">
-    <nav className="flex sm:flex-nowrap space-x-4">
-        <Link to="/creator/creator-events" className="hover:text-rose-500 whitespace-nowrap">Events</Link>
-        <Link to="/creator/creator-events/coming-events" className="hover:text-rose-500 whitespace-nowrap">Coming Events</Link>
-        <Link to="#" className="hover:text-rose-500 whitespace-nowrap">Booked Events</Link>
-        <Link to="/creator/creator-events/summary" className="hover:text-rose-500 whitespace-nowrap" aria-current="page">Summary</Link>
-    </nav>
-</div>
-
+            <div className="p-2 items-center border-b">
+                <nav className="flex sm:flex-nowrap space-x-4">
+                    <Link to="/creator/creator-events" className="hover:text-rose-500 whitespace-nowrap">Events</Link>
+                    <Link to="/creator/creator-events/coming-events" className="hover:text-rose-500 whitespace-nowrap">Coming Events</Link>
+                    <Link to="#" className="hover:text-rose-500 whitespace-nowrap">Booked Events</Link>
+                    <Link to="/creator/creator-events/summary" className="hover:text-rose-500 whitespace-nowrap" aria-current="page">Summary</Link>
+                </nav>
+            </div>
 
             <div className="hidden sm:block col-span-2 py-4 relative items-start">
                 <nav className="space-y-1 px-2 w-fit">
                     <Link to="/creator/creator-events"
                         className="focus:bg-gray-100 mt-5 cursor-pointer py-2 font-semibold transition flex dark:focus:bg-gray-200 hover:bg-gray-100 dark:hover:bg-gray-200 dark:focus:text-black dark:hover:text-gray-700 rounded-md focus:outline-none">
-                        <MdEvent className='h-6 w-6'/>
+                        <MdEvent className='h-6 w-6' />
                         Events
                     </Link>
                     <Link to="/creator/creator-events/coming-events"
                         className="focus:bg-gray-100 mt-5 cursor-pointer py-2 font-semibold transition flex dark:focus:bg-gray-200 hover:bg-gray-100 dark:hover:bg-gray-200 dark:focus:text-black dark:hover:text-gray-700 rounded-md focus:outline-none">
-                        <MdOutlineEventRepeat className='h-6 w-6'/>
+                        <MdOutlineEventRepeat className='h-6 w-6' />
                         Comming Events
                     </Link>
                     <Link className="focus:bg-gray-100 mt-5 cursor-pointer py-2 font-semibold transition flex dark:focus:bg-gray-200 hover:bg-gray-100 dark:hover:bg-gray-200 dark:focus:text-black dark:hover:text-gray-700 rounded-md focus:outline-none">
