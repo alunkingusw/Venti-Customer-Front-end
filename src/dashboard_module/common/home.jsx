78a55ed8254ffcaf48684a32cf5f8640f6f402ac
evanskosgei <evanskosgei090@gmail.com>
2024-09-11 13:55:12 +0300

    spam

--- src/dashboard_module/common/home.jsx
+++ src/dashboard_module/common/home.jsx
@@ -6,7 +6,7 @@ import { GoHeartFill } from "react-icons/go";
 import { IoMdClose } from "react-icons/io";
 import { Error } from '../../components/toasts';
 import InputEmoji from 'react-input-emoji';
-import { X, Heart, MessageCircle, Send, Bookmark, Smile } from 'lucide-react';
+import { CircleX, Heart, MessageCircle, ChevronLeft, Bookmark, Smile } from 'lucide-react';
 
 const Home = () => {
   const [allPosts, setAllPosts] = useState([]);
@@ -140,12 +140,13 @@ const Home = () => {
           </div>
         ))}
       </div>
+
       {commentModal && (
         <div className="fixed inset-0 z-[1055] flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none bg-black/50">
           <div className="relative w-full max-w-6xl max-h-[90vh] mx-4">
             <div className="relative rounded-lg bg-white shadow-lg dark:bg-gray-800">
               <button type="button" onClick={() => setCommentModal(false)} className="absolute top-4 right-4  hover:text-gray-500 dark:hover:text-gray-300 focus:outline-none">
-                <X className="w-6 h-6 font-bold" />
+                <CircleX className="w-6 h-6 font-bold" />
                 <span className="sr-only">Close modal</span>
               </button>
               <div className='hidden sm:block'>
@@ -154,19 +155,26 @@ const Home = () => {
                     <img src={posties.imageUrl} alt="" className="h-full w-full object-fill" />
                   </div>
                   <div className="w-1/2 flex flex-col">
-                      <div className="flex items-center p-4 border-b border-gray-200 dark:border-gray-700">
-                        <img src={postUser.profilePicture} alt="" className="w-10 h-10 rounded-full mr-3" />
+                    <div className="flex items-center p-4 border-b border-gray-200 dark:border-gray-700">
+                      <img src={postUser.profilePicture} alt="" className="w-10 h-10 rounded-full mr-3" />
+                      <span className="font-medium text-gray-800 dark:text-gray-200">{postUser.username}</span>
+                    </div>
+                    <div className="flex pl-4">
+                      <img src={postUser.profilePicture} className="w-10 h-10 rounded-full mr-3" />
+                      <div>
                         <span className="font-medium text-gray-800 dark:text-gray-200">{postUser.username}</span>
+                        <p className="text-gray-600 dark:text-gray-400">{posties.caption}</p>
                       </div>
+                    </div>
                     <div className="flex-1 overflow-y-auto p-4 space-y-4">
-                      {comments.map((comment, index) =>(
-                      <div key={index} className="flex items-center">
-                        <img src="https://picsum.photos/200/300?grayscale" alt="" className="w-10 h-10 rounded-full mr-3" />
-                        <div>
-                          <span className="font-medium text-gray-800 dark:text-gray-200">username</span>
-                          <p className="text-gray-600 dark:text-gray-400">{comment.text}</p>
+                      {comments.map((comment, index) => (
+                        <div key={index} className="flex">
+                          <img src={comment.user.profilePicture} alt="" className="w-10 h-10 rounded-full mr-3" />
+                          <div>
+                            <span className="font-medium text-gray-800 dark:text-gray-200">{comment.user.username}</span>
+                            <p className="text-gray-600 dark:text-gray-400">{comment.text}</p>
+                          </div>
                         </div>
-                      </div>
                       ))}
                     </div>
                     <div className="p-4 border-t border-gray-200 dark:border-gray-700">
@@ -181,7 +189,7 @@ const Home = () => {
                         </div>
                       </div>
                       <p className="font-semibold text-sm text-gray-900 dark:text-white mb-2">1200 likes</p>
-                      <p className="text-xs text-gray-500 dark:text-gray-400 uppercase mb-4">2 days ago</p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400 uppercase mb-4">{new Date(posties.createdAt).toDateString()}</p>
                     </div>
 
                     <div className="p-4 border-t border-gray-200 dark:border-gray-700">
@@ -203,24 +211,37 @@ const Home = () => {
                 </div>
               </div>
 
-              <div className='sm:hidden'>
-                <div className="h-[80vh] max-h-[800px]">
-                  <div className="w-full flex flex-col">
-                    <div className="items-center p-4 border-b border-gray-200 dark:border-gray-700">
-                      <span className="font-bold text-gray-800 dark:text-gray-200">Comments</span>
+              <div className="sm:hidden">
+                <div className="fixed inset-0 bg-white dark:bg-black flex flex-col">
+                  <div className="flex-shrink-0 p-4 border-b border-gray-200 dark:border-gray-700 flex items-center">
+                    <button onClick={()=>setCommentModal(false)} className="text-gray-600 dark:text-gray-400">
+                      <ChevronLeft size={24} />
+                    </button>
+                    <span className="font-bold text-gray-800 dark:text-gray-200 flex-grow text-center">
+                      Comments
+                    </span>
+                  </div>
+                  <div className='flex p-4'>
+                    <img src={postUser.profilePicture} className="w-10 h-10 rounded-full mr-3" />
+                    <div className="flex-grow">
+                      <span className="font-medium text-gray-800 dark:text-gray-200">{postUser.username}</span>
+                      <p className="text-gray-600 dark:text-gray-400 break-words">{posties.caption}</p>
                     </div>
-                    <div className="flex-1 overflow-y-auto p-4 space-y-4">
-                    {comments.map((comment, index) =>(
-                      <div key={index} className="flex items-center">
-                        <img src="https://picsum.photos/200/300?grayscale" alt="" className="w-10 h-10 rounded-full mr-3" />
-                        <div>
-                          <span className="font-medium text-gray-800 dark:text-gray-200">username</span>
-                          <p className="text-gray-600 dark:text-gray-400">{comment.text}</p>
+                  </div>
+
+                  <div className="flex-grow overflow-y-auto p-4 space-y-4">
+                    {comments.map((comment, index) => (
+                      <div key={index} className="flex items-start">
+                        <img src={comment.user.profilePicture} alt="" className="w-10 h-10 rounded-full mr-3 flex-shrink-0" />
+                        <div className="flex-grow">
+                          <span className="font-medium text-gray-800 dark:text-gray-200">{comment.user.username}</span>
+                          <p className="text-gray-600 dark:text-gray-400 break-words">{comment.text}</p>
                         </div>
                       </div>
                     ))}
-                    </div>
+                  </div>
 
+                  <div className="flex-shrink-0 bg-white dark:bg-black">
                     <div className="p-4 border-t border-gray-200 dark:border-gray-700">
                       <div className="flex justify-between mb-4">
                         <div className="flex space-x-4">
@@ -232,20 +253,18 @@ const Home = () => {
                           </button>
                         </div>
                       </div>
-                      <p className="font-semibold text-sm text-gray-900 dark:text-white mb-2">1,234 likes</p>
-                      <p className="text-xs text-gray-500 dark:text-gray-400 uppercase mb-4">2 days ago</p>
+                      <p className="font-semibold text-sm mb-2">1,234 likes</p>
+                      <p className="text-xs uppercase mb-4">2 days ago</p>
                     </div>
 
                     <div className="p-4 border-t border-gray-200 dark:border-gray-700">
                       <div className="flex items-center space-x-3">
-                        {/* <Smile size={24} className="text-gray-400" /> */}
                         <input
                           type="text"
                           placeholder="Add a comment..."
                           className="flex-1 bg-transparent text-gray-800 dark:text-white placeholder-gray-400 focus:outline-none"
                         />
-                        <button
-                          className={`font-semibold text-blue-500 hover:text-blue-600`}>
+                        <button className="font-semibold text-blue-500 hover:text-blue-600">
                           Post
                         </button>
                       </div>
@@ -253,6 +272,7 @@ const Home = () => {
                   </div>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
