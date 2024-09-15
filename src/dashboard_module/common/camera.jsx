f4ab5911f0bca474a8bdc507cd3ec73358dbc593
evanskosgei <evanskosgei090@gmail.com>
2024-09-15 19:40:43 +0300

    torch

--- src/dashboard_module/common/camera.jsx
+++ src/dashboard_module/common/camera.jsx
@@ -1,17 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
 import Webcam from "react-webcam";
-import { useCallback, useRef, useState } from "react";
+import { useTorchLight } from '@blackbox-vision/use-torch-light';
+import React, { useCallback, useRef, useState } from "react";
 import { FaCamera, FaUndo, FaSync } from 'react-icons/fa';
-import { RxCross2 } from "react-icons/rx";
-import { Check, MessageCircle, Trash2, Search, Mail, Phone, Pencil, CircleUserRound, Grid2X2 } from 'lucide-react';
+import { Check, X, Trash2, Search, Mail, Phone, Pencil, CircleUserRound, Grid2X2 } from 'lucide-react';
 
 const Camera = ({ closeCamera }) => {
     const webcamRef = useRef(null);
+    const streamRef = useRef(null);
     const [imgSrc, setImgSrc] = useState(null);
     const [mirrored, setMirrored] = useState(true);
     const [currentDiv, setCurrentDiv] = useState('takeImage');
     const [facingMode, setFacingMode] = useState("user");
+    const [on, toggle] = useTorchLight(streamRef.current);
 
     const videoConstraints = {
         aspectRatio: 4 / 3,
@@ -32,14 +33,18 @@ const Camera = ({ closeCamera }) => {
         setMirrored(prevMirrored => !prevMirrored);
     };
 
+    const setRef = ({ stream }) => {
+        streamRef.current = stream;
+      };
+
     return (
         <>
             {currentDiv === 'takeImage' && (
                 <div className="fixed sm:hidden inset-0 z-50 flex flex-col overflow-hidden items-center max-h-screen">
                     <button
                         onClick={closeCamera}
-                        type="button" className="text-gray-400 m-6 fixed inset-0 z-10 bg-transparent text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-                        <RxCross2 className="w-10 h-10 font-extrabold text-cyan-100" aria-hidden="true" />
+                        type="button" className="text-gray-400 right-2 m-6 fixed inset-0 z-10 bg-transparent text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                        <X className="w-10 h-10 font-extrabold text-gray-100" aria-hidden="true" />
                         <span className="sr-only">Close modal</span>
                     </button>
                     <div className="relative overflow-hidden flex top-0 bottom-0 h-screen  w-full border-4">
@@ -74,6 +79,7 @@ const Camera = ({ closeCamera }) => {
                                 </div>
                             ) : (
                                 <>
+                                 <button onClick={toggle}>{on ? 'Disable Torch' : 'Enable Torch'}</button>
                                     <button
                                         onClick={capture}
                                         className="p-4 text-gray-800 border border-black bg-gray-100 rounded-full transition duration-300 flex items-center text-lg">
