e7d124fa33bd672cc5fb7bf9be9b5fdf50433789
evanskosgei <evanskosgei090@gmail.com>
2024-09-15 18:43:27 +0300

    ooh

--- src/dashboard_module/common/camera.jsx
+++ src/dashboard_module/common/camera.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import Webcam from "react-webcam";
 import { useCallback, useRef, useState } from "react";
 import { FaCamera, FaUndo, FaSync } from 'react-icons/fa';
 import { RxCross2 } from "react-icons/rx";
+import { Check, MessageCircle, Trash2, Search, Mail, Phone, Pencil, CircleUserRound, Grid2X2 } from 'lucide-react';
 
 const Camera = ({ closeCamera }) => {
     const webcamRef = useRef(null);
@@ -34,10 +36,10 @@ const Camera = ({ closeCamera }) => {
         <>
             {currentDiv === 'takeImage' && (
                 <div className="fixed sm:hidden inset-0 z-50 flex flex-col overflow-hidden items-center max-h-screen">
-                    <button 
-                    onClick={closeCamera}
-                    type="button" className="text-gray-400 m-6 fixed inset-0 z-10 bg-transparent text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-                        <RxCross2 className="w-10 h-10 font-extrabold text-cyan-100" aria-hidden="true"/>
+                    <button
+                        onClick={closeCamera}
+                        type="button" className="text-gray-400 m-6 fixed inset-0 z-10 bg-transparent text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                        <RxCross2 className="w-10 h-10 font-extrabold text-cyan-100" aria-hidden="true" />
                         <span className="sr-only">Close modal</span>
                     </button>
                     <div className="relative overflow-hidden flex top-0 bottom-0 h-screen  w-full border-4">
@@ -60,11 +62,16 @@ const Camera = ({ closeCamera }) => {
                         )}
                         <div className="absolute z-10 justify-center items-center bottom-4 left-1/2 transform -translate-x-1/2 flex gap-4">
                             {imgSrc ? (
-                                <button
-                                    onClick={retake}
-                                    className="p-4 text-black rounded-full border border-red-600 bg-gray-100 transition duration-300 flex items-center text-lg">
-                                    <FaUndo className="w-8 h-8" />
-                                </button>
+                                <div className="flex space-x-6 items-center">
+                                    <button
+                                        onClick={retake}
+                                        className="p-4 text-black rounded-full border border-red-600 bg-gray-100 transition duration-300 flex items-center text-lg">
+                                        <FaUndo className="w-8 h-8" />
+                                    </button>
+                                    <button className="p-4 text-white rounded-full border bg-rose-500 transition duration-300 flex items-center text-lg">
+                                        <Check className="w-6 h-6" />
+                                    </button>
+                                </div>
                             ) : (
                                 <>
                                     <button
